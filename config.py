b0fcb375c4de93fe321cb4006e510f9b248f3eaa
personaMAD <151635412+personaMAD@users.noreply.github.com>
2023-11-21 15:06:15 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+seed = '' #пишем сид от метамаска
+passwd = '' #пароль для ММ, который будет устанавливаться в хромдрайвере
+
+chainid = '1088'
+currency = 'METIS'
+rpc = 'https://metis-mainnet.public.blastapi.io'
+network = 'Metis'
